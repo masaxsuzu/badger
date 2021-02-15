eea14a962c6f64f155cc8b1a62aae102bc8bb9da
masaxsuzu <masaxsuzu+github@gmail.com>
2021-02-15 20:24:46 +0900

    Complete project 04
    - Complete project 04/mult
    - Complete project 04/fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,85 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+//  state = 0;
+//  while (true) {
+//      if(key) {
+//          color = 1;
+//      }   else {
+//          color = 0;
+//      }
+//      if state == color continue;
+//      state = color;
+//      for pixel in screen {
+//          pixel = color;
+//      }
+//  }
+
+    @8192
+    D=A
+    @number_of_pixel
+    M=D
+    @R0
+    M=0
+(CHECK_KEY)
+    @KBD
+    D=M
+    @KEY_NOT_PRESSED
+    D;JEQ
+(KEY_PRESSED)
+    D=M
+    @R1
+    M=1
+    @CHECK_STATE
+    D;JMP
+(KEY_NOT_PRESSED)
+    D=M
+    @R1
+    M=0
+    @CHECK_STATE
+    D;JMP
+(CHECK_STATE)
+    @R0
+    D=M
+    @R1
+    D=D-M
+    @CHECK_KEY
+    D;JEQ
+    @i
+    M=0;
+    @R1
+    D=M
+    @R0
+    M=D
+    @FILL_WHITE
+    D;JEQ
+    @FILL_BLACK
+    D;JMP
+(FILL_WHITE)
+    @i
+    D=M
+    @number_of_pixel
+    D=M-D
+    @CHECK_KEY
+    D;JLT
+    @SCREEN
+    A=A+D
+    M=0
+    @i
+    MD=M+1
+    @FILL_WHITE
+    0;JMP
+(FILL_BLACK)
+    @i
+    D=M
+    @number_of_pixel
+    D=M-D
+    @CHECK_KEY
+    D;JLT
+    @SCREEN
+    A=A+D
+    M=-1
+    @i
+    MD=M+1
+    @FILL_BLACK
+    0;JMP
