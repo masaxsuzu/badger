7c68563f21df324f9686b34442bf93ce3cb28cba
masaxsuzu <masaxsuzu+github@gmail.com>
2021-09-02 20:11:54 +0900

    tmp

---
+++ projects/10/src/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Netsoft.Badger.Compiler.Frontend
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var filePath = args[0];
+            var content = System.IO.File.ReadAllText(filePath);
+            var tokens = Tokenize(content);
+            var ast = Parse(tokens);
+            System.Console.WriteLine(ast);
+            
+            return 0;
+        }
+
+        static object Tokenize(string content){
+            return content;
+        }
+        static object Parse(object tokens){
+            return tokens;
+        }
+    }
+}
---
+++ projects/10/src/test.ps1
@@ -0,0 +1,1 @@
+dotnet run --project .\Netsoft.Badger.Compiler.Frontend.csproj ..\ArrayTest\Main.jack
